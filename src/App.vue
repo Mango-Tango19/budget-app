4d6e7765daac0d85da359505846d9fd73c0eccf5
Dasha <thisisdasha@gmail.com>
2021-02-13 16:02:46 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <BudgetList :list="list" />
+  </div>
+</template>
+
+<script>
+import BudgetList from "./components/BudgetList";
+
+export default {
+  name: "App",
+  components: {
+    BudgetList,
+  },
+  data: () => ({
+    list: {
+      1: {
+        type: "INCOME",
+        value: 100,
+        comments: "Income coment",
+        id: 1,
+      },
+      2: {
+        type: "OUTCOME",
+        value: -50,
+        comments: "Outcome coment",
+        id: 2,
+      },
+    },
+  }),
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BudgetList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="budget-list-wrap">
+    <el-card :header="header">
+      <template v-if="!isEmpty">
+        <div class="list-item" v-for="(item, prop) in list" :key="prop">
+          <span class="budget-comment">{{ item.comments }}</span>
+          <span class="budget-value">{{ item.value }}</span>
+          <el-button type="danger" size="mini">delete</el-button>
+        </div>
+      </template>
+      <el-alert v-else :title="title" type="info" :closable="false"> </el-alert>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BudgetList",
+  props: {
+    list: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data: () => ({
+    header: "Budget List",
+    title: "Empty List",
+  }),
+  computed: {
+     isEmpty() {
+      return !Object.keys(this.list).length 
+  }
+}
+}
+</script>
+
+<style scooped>
+.budget-list-wrap {
+  max-width: 500px;
+  margin: auto;
+}
+
+.list-item {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+  justify-content: center;
+}
+
+.budget-value {
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: 20px;
+}
+</style>
